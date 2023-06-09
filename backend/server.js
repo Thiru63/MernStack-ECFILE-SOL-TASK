ae26c4d70a71dc46b4d5a4018c640a3c203dcb47
Thiru63 <100009930+Thiru63@users.noreply.github.com>
2023-06-09 19:34:38 +0530

    dp

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 const cors=require('cors');
 const bodyParser = require('body-parser');
 const { errorHandler } = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 
 
@@ -23,6 +24,23 @@ app.use(bodyParser.json())
 
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>{
+      res.sendFile(
+        path.join(__dirname, "../frontend/build/index.html"),
+        function (err){
+            res.status(500).send(err);
+        }
+      );
+});
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 app.use(errorHandler);
 
